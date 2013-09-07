b284b433845c9ecd254db3f62dcd605a5922e5e4
Terry Wang <i@terry.im>
2013-09-07 13:03:23 +1000

    add Oracle Linux 5.9 x86 veewee definition

---
+++ veewee/definitions/OracleLinux-5.9-i386-DVD/proxy.sh
@@ -0,0 +1,4 @@
+# Set proxy for machines behind firewall
+export http_proxy="http://www-proxy.au.oracle.com:80"
+export https_proxy="http://www-proxy.au.oracle.com:80"
+export ftp_proxy="http://www-proxy.au.oracle.com:80"
