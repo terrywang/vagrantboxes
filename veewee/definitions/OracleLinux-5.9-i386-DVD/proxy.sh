a45390ad0b92349f095651d8207be6370f5665d7
Terry Wang <i@terry.im>
2013-09-07 13:09:09 +1000

    remove proxy server information -_-z

--- veewee/definitions/OracleLinux-5.9-i386-DVD/proxy.sh
+++ veewee/definitions/OracleLinux-5.9-i386-DVD/proxy.sh
@@ -1,4 +1,4 @@
 # Set proxy for machines behind firewall
-export http_proxy="http://www-proxy.au.oracle.com:80"
-export https_proxy="http://www-proxy.au.oracle.com:80"
-export ftp_proxy="http://www-proxy.au.oracle.com:80"
+# export http_proxy=""
+# export https_proxy=""
+# export ftp_proxy=""
