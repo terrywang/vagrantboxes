47e6cf70ca04adf44f67ab3c1fd6e50f1b37f940
Terry Wang <i@terry.im>
2014-09-02 22:54:22 +1000

    fix epel-release package name in base.sh

--- veewee/definitions/OracleLinux-7.0-x86_64-DVD/base.sh
+++ veewee/definitions/OracleLinux-7.0-x86_64-DVD/base.sh
@@ -6,8 +6,8 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
 cd /tmp
 wget http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
-rpm -ivh epel-release-7-0.2.noarch.rpm
-rm -f epel-release-7-0.2.noarch.rpm
+rpm -ivh epel-release-7-1.noarch.rpm 
+rm -f epel-release-7-1.noarch.rpm
 # Not flexible to switch between direct Internet access and behind firewall
 # --httpproxy HOST --httpport PORT
 # rpm -ivh http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-1.noarch.rpm
